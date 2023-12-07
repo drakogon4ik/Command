e9f1e74eb0ffde0b07df0cb968c5892797444934
drakogon4ik <94756197+drakogon4ik@users.noreply.github.com>
2023-12-07 21:44:34 +0300

    Clear server.py

--- server.py
+++ server.py
@@ -1,82 +1,128 @@
 """
-Author:
-Date:
-Description
+Author: Oleg Shkolnik יא9.
+Description: Server that makes one of available commands (NAME/TIME/RAND/EXIT) and sends result to client
+Date: 7/12/23
 """
+
 import socket
 from datetime import datetime
 import random
 
 MAX_PACKET = 1024
-
 QUEUE_LEN = 1
+IP = "127.0.0.1"
+PORT = 5746
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-n = 0
-try:
+COMMANDS = ['NAME', 'TIME', 'RAND', 'EXIT']
+ERR = 'Invalid command. Available commands are: NAME/TIME/RAND/EXIT:'
 
-    # Set up server
-    my_socket.bind(('127.0.0.1', 5746))
 
-    my_socket.listen(QUEUE_LEN)
+def validate_command(request: str):
+    """
+    Validates that the user input is valid
+    :param request: the user command
+    :return: true if the command is valid
+    """
+    return request in COMMANDS
 
-    client_socket, client_address = my_socket.accept()
+
+def do_functions(request: str):
+    """
+    Return action that user wants
+    :param request: the user command
+    :return: action depending on the command goal
+    """
+    if request == COMMANDS[0]:
+        result = "The name of server is ObnS's server"
+
+    elif request == COMMANDS[1]:
+        result = datetime.now().time()
+
+    elif request == COMMANDS[2]:
+        result = random.randint(1, 10)
+    else:
+        result = False
+    return result
+
+
+def main():
+    """
+    Setting ip and port
+    """
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        """
+        Set upping server
+        """
+        my_socket.bind((IP, PORT))
+        my_socket.listen(QUEUE_LEN)
 
         while True:
+            """
+            Server is always ready to connect new client
+            """
+            client_socket, client_address = my_socket.accept()
 
-            # If server see that we dont put nothing a lot of time, reconnecting it
-            if n > 1024:
+            try:
 
-                client_socket, client_address = my_socket.accept()
-                n = 0
+                while True:
+                    """
+                    Getting request
+                    Making command
+                    Sending result of command execution
+                    """
 
-            # Getting request
-            request = client_socket.recv(MAX_PACKET).decode()
-            print('server received ' + request)
+                    request = client_socket.recv(MAX_PACKET).decode()
+                    print('server received ' + request)
 
-            # Calculating every time we skip
-            if request == '':
-                n += 1
+                    if validate_command(request):
+                        response = str(do_functions(request))
 
-            # Making commands and sending them
-            elif request == "TIME":
-                response = str(datetime.now().time())
-                client_socket.send(response.encode())
+                    elif request == 'EXIT':
+                        break
 
-            elif request == "NAME":
-                response = "The name of server is ObnS's server"
-                client_socket.send(response.encode())
+                    else:
+                        response = ERR
 
-            elif request == "RAND":
-                response = str(random.randint(1, 10))
-                client_socket.send(response.encode())
+                    client_socket.send(response.encode())
 
-            elif request == "EXIT":
-                response = "bye"
-                client_socket.send(response.encode())
+            except socket.error as err:
+                """
+                Send the name of error in error situation
+                """
+                print('received socket error on client socket' + str(err))
 
-            else:
-                response = 'Command was written not right. Please try again.'
-                client_socket.send(response.encode())
+            finally:
+                """
+                Close the socket anyway
+                """
+                client_socket.close()
 
-    # If we have any errors
     except socket.error as err:
+        """
+        Send the name of error in error situation
+        """
+        print('received socket error on server socket' + str(err))
 
-        print('received socket error on client socket' + str(err))
-
-    # Closing client
     finally:
+        """
+        Close the socket anyway
+        """
+        my_socket.close()
 
-        client_socket.close()
 
-# If we have any errors
-except socket.error as err:
-
-    print('received socket error on server socket' + str(err))
-
-# Closing the program any way
-finally:
-
-    my_socket.close()
+if __name__ == '__main__':
+    """
+    checking function situations and launching the main
+    """
+    assert validate_command('EXIT')
+    assert validate_command('NAME')
+    assert validate_command('RAND')
+    assert validate_command('TIME')
+    assert not validate_command('Oleg')
+    assert do_functions('NAME')
+    assert do_functions('RAND')
+    assert do_functions('TIME')
+    assert not do_functions('EXIT')
+    main()
