66a61c10b31e7bcb097374a0a8b439d317f32fd6
drakogon4ik <94756197+drakogon4ik@users.noreply.github.com>
2023-12-07 21:45:29 +0300

    Clear client.py

--- client.py
+++ client.py
@@ -1,19 +1,22 @@
 """
 Author: Oleg Shkolnik יא9.
-Description: Client that receive input commands from cmd, send them to the server, receive result and print it.
-Date: 6/12/23
+Description: Client that receives input commands from cmd, sends them to the server, receives result and prints it
+Date: 7/12/23
 """
+
 import socket
 
 IP = "127.0.0.1"
 PORT = 5746
+
 PROMPT = 'Please enter a command(NAME/TIME/RAND/EXIT): '
+ERR = 'Invalid command. Available commands are: NAME/TIME/RAND/EXIT:'
 COMMANDS = ['NAME', 'TIME', 'RAND', 'EXIT']
 
 
 def validate_command(cmd: str):
     """
-    validates that the user input is valid
+    Validates that the user input is valid
     :param cmd: the user command
     :return: true if the command is valid
     """
@@ -22,60 +25,59 @@ def validate_command(cmd: str):
 
 def main():
     """
-    main function that connect client to server
+    Main function that connect client to server
     """
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         """
-        connecting
+        Connecting
         """
         my_socket.connect((IP, PORT))
         request = input(PROMPT)
 
         while request != COMMANDS[3]:
             """
-            loop until getting EXIT
+            Loop until getting EXIT
             """
 
             if validate_command(request):
                 """
-                if command exist - continue
-                if not - send message with available functions and go to loop from the start
+                If command exist - continue
+                If not - send message with available functions and go to loop from the start
                 """
 
-                if request != '':
-                    """
-                    sending request to the server end receive answer
-                    """
-                    my_socket.send(request.encode())
-                    data = my_socket.recv(1024).decode()
-                    print(data)
+                my_socket.send(request.encode())
+                data = my_socket.recv(1024).decode()
+                print(data)
 
             else:
-                print('invalid command. available commands are: NAME/TIME/RAND/EXIT:')
+                print(ERR)
 
             request = input(PROMPT)
 
         """
-        after the loop send message and go to finally closing socket
+        After the loop send message and go to finally closing socket
         """
         print('bye bye')
 
     except socket.error as err:
         """
-        for errors
+        Send the name of error in error situation
         """
         print('received socket error ' + str(err))
 
     finally:
         """
-        close the socket anyway
+        Close the socket anyway
         """
         my_socket.close()
 
 
 if __name__ == '__main__':
+    """
+    checking function situations and launching the main
+    """
     assert validate_command('EXIT')
     assert validate_command('NAME')
     assert validate_command('RAND')
